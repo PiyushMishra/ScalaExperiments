4e81aab24e46aea2dd4b82529a88624899cd18cb
Meetu Maltiar <meetu.maltiar@gmail.com>
2011-03-19 23:45:09 +0530

    Learnt the type inference and implementing a method

--- src/main/scala/com/meetu/scalaexperiments/App.scala
+++ src/main/scala/com/meetu/scalaexperiments/App.scala
@@ -1,11 +1,22 @@
 package com.meetu.scalaexperiments
 
 /**
- * @author ${user.name}
+ * @author ${Meetu Maltiar}
  */
 object App {
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    // just calling a function without type inference
+    var resultWithoutTypeInference :Int = max(3,4);
+    println( "Maximum of two numbers with type inference is: " + resultWithoutTypeInference)
+    
+    // well this is an example of type inference
+    var resultWithTypeInference = max(2,3);
+    println( "Maximum of two numbers with type inference is: " + resultWithTypeInference)
+  }
+
+  def max(x:Int, y:Int): Int = {
+    if(x > y) x
+    else y
   }
 }
